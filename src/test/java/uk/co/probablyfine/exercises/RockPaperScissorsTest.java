e517d78d7954facdad643518d3c8efb050e4b14f
Alex Wilson <gpg@probablyfine.co.uk>
2019-11-02 17:06:26 +0000

    Initial commit

---
+++ src/test/java/uk/co/probablyfine/exercises/RockPaperScissorsTest.java
@@ -0,0 +1,14 @@
+package uk.co.probablyfine.exercises;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class RockPaperScissorsTest {
+
+    @Test
+    public void first() {
+        assertThat(1, is(1));
+    }
+}
