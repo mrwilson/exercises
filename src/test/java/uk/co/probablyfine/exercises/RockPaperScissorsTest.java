b8fb0cac3113c658fd77584f8a2a3c13a9365210
Alex Wilson <gpg@probablyfine.co.uk>
2019-11-10 19:41:02 +0000

    Rock-Paper-Scissors

--- src/test/java/uk/co/probablyfine/exercises/RockPaperScissorsTest.java
+++ src/test/java/uk/co/probablyfine/exercises/RockPaperScissorsTest.java
@@ -15,13 +15,16 @@ public class RockPaperScissorsTest {
     @Test
     public void second() {
         assertThat(play("PAPER","ROCK"), is("P1"));
+        assertThat(play("ROCK","PAPER"), is("P2"));
     }
 
     private Object play(String move1, String move2) {
         if("PAPER".equals(move1) && "ROCK".equals(move2)) {
             return "P1";
+        } else if ("ROCK".equals(move1) && "PAPER".equals(move2)) {
+            return "P2";
+        } else {
+            return "DRAW";
         }
-
-        return "DRAW";
     }
 }
