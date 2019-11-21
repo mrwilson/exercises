a80e9da09dbabbb5a9583dd94944d5fe27210ed2
Alex Wilson <gpg@probablyfine.co.uk>
2019-11-21 22:43:10 +0000

    Sorting-Characters

--- src/test/java/uk/co/probablyfine/exercises/SortingCharactersTest.java
+++ src/test/java/uk/co/probablyfine/exercises/SortingCharactersTest.java
@@ -2,6 +2,10 @@ package uk.co.probablyfine.exercises;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -17,7 +21,16 @@ public class SortingCharactersTest {
         assertThat(sort("aA"), is("aa"));
     }
 
+    @Test
+    public void capsDifferentCharacters() {
+        assertThat(sort("Ba"), is("ab"));
+    }
+
     private String sort(String input) {
-        return input.toLowerCase();
+        return input.toLowerCase()
+            .chars()
+            .sorted()
+            .mapToObj(Character::toString)
+            .collect(Collectors.joining());
     }
 }
