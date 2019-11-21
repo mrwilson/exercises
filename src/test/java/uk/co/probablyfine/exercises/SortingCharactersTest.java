4f3ca2f1f5de53447a297d5bc543454a4daec313
Alex Wilson <gpg@probablyfine.co.uk>
2019-11-21 23:58:37 +0000

    Making initial Sorting-Characters implementation static

--- src/test/java/uk/co/probablyfine/exercises/SortingCharactersTest.java
+++ src/test/java/uk/co/probablyfine/exercises/SortingCharactersTest.java
@@ -2,9 +2,7 @@ package uk.co.probablyfine.exercises;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -31,7 +29,7 @@ public class SortingCharactersTest {
         assertThat(sort("When not studying nuclear physics, Bambi likes to play beach volleyball."), is("aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy"));
     }
 
-    private String sort(String input) {
+    static String sort(String input) {
         return input.toLowerCase()
             .chars()
             .filter(character -> 'A' <= character && character <= 'z')
