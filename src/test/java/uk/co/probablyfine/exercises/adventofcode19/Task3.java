922598dc2058f16a4a16c049da7e39979f83f7d9
Alex Wilson <gpg@probablyfine.co.uk>
2019-12-08 18:04:24 +0000

    AdventOfCode19

---
+++ src/test/java/uk/co/probablyfine/exercises/adventofcode19/Task3.java
@@ -0,0 +1,67 @@
+package uk.co.probablyfine.exercises.adventofcode19;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Task3 {
+
+    @Test
+    public void noOps() {
+        int[][] wire1Points = {
+                {0, 0},
+                {1, 0},
+                {1, 1}
+        };
+
+        int[][] wire2Points = {
+                {0, 0},
+                {0, 1},
+                {1, 1}
+        };
+
+        assertThat(wireCross(wire1Points, wire2Points), is(2));
+
+    }
+
+    private int wireCross(int[][] wire1Points, int[][] wire2Points) {
+        Stream<int[]> wire1 = Arrays.stream(wire1Points);
+        Stream<int[]> wire2 = Arrays.stream(wire2Points);
+
+        return Stream.concat(wire1, wire2)
+        .map(this::toPair)
+        .distinct()
+        .mapToInt(pair -> pair.x + pair.y)
+        .max()
+        .orElse(0);
+
+    }
+
+    private Pair toPair(int[] ints) {
+        return new Pair(
+            ints[0],
+            ints[1]
+        );
+    }
+
+    static class Pair {
+        final int x;
+        final int y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+}
