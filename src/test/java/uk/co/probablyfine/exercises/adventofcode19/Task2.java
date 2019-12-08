a302046242254bf60fd3b7a9be342e7de8b1aa03
Alex Wilson <gpg@probablyfine.co.uk>
2019-12-08 17:20:19 +0000

    AdventOfCode19

--- src/test/java/uk/co/probablyfine/exercises/adventofcode19/Task2.java
+++ src/test/java/uk/co/probablyfine/exercises/adventofcode19/Task2.java
@@ -2,6 +2,8 @@ package uk.co.probablyfine.exercises.adventofcode19;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -14,7 +16,23 @@ public class Task2 {
         assertThat(runIntcode(input), is(input));
     }
 
+    @Test
+    public void addition() {
+        int[] input = {1, 0, 0, 0};
+
+        int[] output = runIntcode(input);
+
+        assertThat(output[0], is(2));
+    }
+
     private int[] runIntcode(int[] input) {
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == 1) {
+                input[input[i+3]] = input[input[i+1]] + input[input[i+2]];
+            }
+        }
+
         return input;
     }
 }
