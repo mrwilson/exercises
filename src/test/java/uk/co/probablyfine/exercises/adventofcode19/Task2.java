1cb1458a0858f188415784f18500a47f5fa52537
Alex Wilson <gpg@probablyfine.co.uk>
2019-12-08 17:39:21 +0000

    AdventOfCode19

--- src/test/java/uk/co/probablyfine/exercises/adventofcode19/Task2.java
+++ src/test/java/uk/co/probablyfine/exercises/adventofcode19/Task2.java
@@ -99,12 +99,13 @@ public class Task2 {
         loop: for (int i = 0; i < input.length; i += 4) {
 
             switch(input[i]) {
+
                 case Operation.ADD:
-                    input[input[i+3]] = input[input[i+1]] + input[input[i+2]];
-                    break;
+                    input[input[i+3]] = input[input[i+1]] + input[input[i+2]]; break;
+
                 case Operation.MULTIPLY:
-                    input[input[i+3]] = input[input[i+1]] * input[input[i+2]];
-                    break;
+                    input[input[i+3]] = input[input[i+1]] * input[input[i+2]]; break;
+
                 case Operation.HALT:
                 default:
                     break loop;
