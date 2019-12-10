6ecc4aa7e8c7a071b091a4d719359a326678ebd8
Alex Wilson <gpg@probablyfine.co.uk>
2019-12-10 19:46:58 +0000

    AdventOfCode19

--- src/test/java/uk/co/probablyfine/exercises/adventofcode19/Task5.java
+++ src/test/java/uk/co/probablyfine/exercises/adventofcode19/Task5.java
@@ -2,6 +2,7 @@ package uk.co.probablyfine.exercises.adventofcode19;
 
 import org.junit.Test;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -29,5 +30,18 @@ public class Task5 {
 
         assertThat(output.get(), is(99));
     }
+
+    @Test
+    public void testLoadAndStore() {
+        int[] program = {3, 0, 4, 0, 99};
+
+        int input = new Random().nextInt();
+        AtomicInteger output = new AtomicInteger(0);
+
+        runIntcode(program, input, output::set);
+
+        assertThat(output.get(), is(input));
+    }
+
 
 }
