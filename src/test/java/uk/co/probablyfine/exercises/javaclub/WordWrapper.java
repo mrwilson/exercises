ce3ca6cb59390c631442f5ef83ed57d48cf66f5f
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2020-04-14 16:19:33 +0100

    feat(word-wrap): Zero case, let's say is no breaks

--- src/test/java/uk/co/probablyfine/exercises/javaclub/WordWrapper.java
+++ src/test/java/uk/co/probablyfine/exercises/javaclub/WordWrapper.java
@@ -1,5 +1,20 @@
 package uk.co.probablyfine.exercises.javaclub;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class WordWrapper {
+
+    @Test
+    public void zeroWidthColumnIsNoBreaks() {
+        assertThat(wrap("a b", 0), is("a b"));
+
+    }
+
+    private static String wrap(String input, int columns) {
+        return input;
+    }
 
 }
