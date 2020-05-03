1f7f997eb0720a8bc83301617528fbfa74b37097
Alex Wilson <gpg@probablyfine.co.uk>
2020-05-03 11:47:45 +0100

    feat(dice): Add randomness as a stubbed stream of doubles

--- src/test/java/uk/co/probablyfine/exercises/DiceRollerTest.java
+++ src/test/java/uk/co/probablyfine/exercises/DiceRollerTest.java
@@ -2,6 +2,14 @@ package uk.co.probablyfine.exercises;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -15,9 +23,27 @@ public class DiceRollerTest {
         assertThat(roll("3d1"), is(3));
     }
 
+    @Test
+    public void twoSidedDiceRequireRandomness() {
+        assertThat(roll("1d2", DoubleStream.of(0.0d)), is(1));
+        assertThat(roll("2d2", DoubleStream.of(0.0d, 0.9d)), is(3));
+        assertThat(roll("3d2", DoubleStream.of(0.0d, 0.9d, 0.1d)), is(4));
+    }
+
     private int roll(String dice) {
+        return roll(dice, new Random().doubles(0.0d, 1.0d));
+    }
+
+    private int roll(String dice, DoubleStream randomness) {
         String[] arguments = dice.split("");
 
-        return Integer.parseInt(arguments[0]);
+
+        int numberOfRolls = Integer.parseInt(arguments[0]);
+        int numberOfSides = Integer.parseInt(arguments[2]);
+
+        return randomness
+                .limit(numberOfRolls)
+                .mapToInt(i -> (int) Math.round(Math.floor(i * numberOfSides)) + 1)
+                .sum();
     }
 }
