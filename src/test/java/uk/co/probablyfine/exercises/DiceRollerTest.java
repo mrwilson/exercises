1aee7c17118ad42f7bfd15fb43867a48b48072d2
Alex Wilson <gpg@probablyfine.co.uk>
2020-05-03 12:58:08 +0100

    refactor(dice): Supply a DoubleStream to enable re-use later

--- src/test/java/uk/co/probablyfine/exercises/DiceRollerTest.java
+++ src/test/java/uk/co/probablyfine/exercises/DiceRollerTest.java
@@ -3,6 +3,7 @@ package uk.co.probablyfine.exercises;
 import org.junit.Test;
 
 import java.util.Random;
+import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.DoubleStream;
@@ -21,15 +22,15 @@ public class DiceRollerTest {
 
     @Test
     public void twoSidedDiceRequireRandomness() {
-        assertThat(roll("1d2", DoubleStream.of(0.0d)), is(1));
-        assertThat(roll("2d2", DoubleStream.of(0.0d, 0.9d)), is(3));
-        assertThat(roll("3d2", DoubleStream.of(0.0d, 0.9d, 0.1d)), is(4));
+        assertThat(roll("1d2", supplyDoubles(0.0d)), is(1));
+        assertThat(roll("2d2", supplyDoubles(0.0d, 0.9d)), is(3));
+        assertThat(roll("3d2", supplyDoubles(0.0d, 0.9d, 0.1d)), is(4));
     }
 
     @Test
     public void doubleDigitSidesAndNumberOfRolls() {
-        assertThat(roll("10d10", repeat(0.0d)), is(10));
-        assertThat(roll("10d10", repeat(0.9d)), is(100));
+        assertThat(roll("10d10", repeatDoubles(0.0d)), is(10));
+        assertThat(roll("10d10", repeatDoubles(0.9d)), is(100));
     }
 
     @Test
@@ -50,10 +51,10 @@ public class DiceRollerTest {
     }
 
     private int roll(String dice) {
-        return roll(dice, new Random().doubles(0.0d, 1.0d));
+        return roll(dice, () -> new Random().doubles(0.0d, 1.0d));
     }
 
-    private int roll(String dice, DoubleStream randomness) {
+    private int roll(String dice, Supplier<DoubleStream> randomness) {
         Matcher matcher = Pattern.compile("(\\d+)d(\\d+)(([+-\\\\*])(\\d+))?").matcher(dice);
 
         if (!matcher.find()) {
@@ -64,6 +65,7 @@ public class DiceRollerTest {
         int numberOfSides = Integer.parseInt(matcher.group(2));
 
         int sum = randomness
+                .get()
                 .limit(numberOfRolls)
                 .mapToInt(i -> (int) Math.round(Math.floor(i * numberOfSides)) + 1)
                 .sum();
@@ -83,8 +85,12 @@ public class DiceRollerTest {
 
     }
 
-    private DoubleStream repeat(double v) {
-        return DoubleStream.iterate(v, i -> i);
+    private Supplier<DoubleStream> repeatDoubles(double value) {
+        return () -> DoubleStream.iterate(value, i -> i);
+    }
+
+    private Supplier<DoubleStream> supplyDoubles(double... values) {
+        return () -> DoubleStream.of(values);
     }
 
     private static class InvalidDiceRollException extends RuntimeException {
