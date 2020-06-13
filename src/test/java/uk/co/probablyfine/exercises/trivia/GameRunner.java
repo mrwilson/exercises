98578bfde8abb5637130528aeb92920fec37d77f
Alex Wilson <gpg@probablyfine.co.uk>
2020-06-13 15:14:58 +0100

    feat(trivia): Re-order method to enable extraction

--- src/test/java/uk/co/probablyfine/exercises/trivia/GameRunner.java
+++ src/test/java/uk/co/probablyfine/exercises/trivia/GameRunner.java
@@ -7,16 +7,15 @@ public class GameRunner {
 	private static boolean notAWinner;
 
 	public static void main(String[] args) {
+		Random rand = new Random();
+
 		Game aGame = new Game();
 		
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
 		
-		Random rand = new Random();
-	
 		do {
-			
 			aGame.roll(rand.nextInt(5) + 1);
 			
 			if (rand.nextInt(9) == 7) {
@@ -24,9 +23,7 @@ public class GameRunner {
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
 		} while (notAWinner);
 		
 	}
