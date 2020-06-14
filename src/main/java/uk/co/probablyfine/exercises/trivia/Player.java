ad6fc3e9aabee52b282cacf280899013fb360c0e
Alex Wilson <gpg@probablyfine.co.uk>
2020-06-14 15:16:22 +0100

    feat(trivia): Allow a "Player" to "moveForward" based on their roll

--- src/main/java/uk/co/probablyfine/exercises/trivia/Player.java
+++ src/main/java/uk/co/probablyfine/exercises/trivia/Player.java
@@ -1,4 +1,7 @@
 package uk.co.probablyfine.exercises.trivia;
 
 public record Player(String name, int place) {
+    public Player moveForward(int roll) {
+        return new Player(name, (this.place + roll) % 12);
+    }
 }
