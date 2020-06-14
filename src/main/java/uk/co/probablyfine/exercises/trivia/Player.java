af95acee71cb9e60d75a8cc782f663485f38786a
Alex Wilson <gpg@probablyfine.co.uk>
2020-06-14 15:39:17 +0100

    refactor(trivia): Add name-only constructor to "Player" with default place and coins of 0

--- src/main/java/uk/co/probablyfine/exercises/trivia/Player.java
+++ src/main/java/uk/co/probablyfine/exercises/trivia/Player.java
@@ -4,4 +4,8 @@ public record Player(String name, int place, int coins) {
     public Player moveForward(int roll) {
         return new Player(name, (this.place + roll) % 12, coins);
     }
+
+    public Player(String name) {
+        this(name, 0, 0);
+    }
 }
