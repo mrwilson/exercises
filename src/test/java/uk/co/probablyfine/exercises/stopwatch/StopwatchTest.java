7de44d948536773f48513843b3f388d6f016dd7b
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2020-07-01 11:29:07 +0100

    feat(stopwatch): Introduce TimeProvider and take display after one second

--- src/test/java/uk/co/probablyfine/exercises/stopwatch/StopwatchTest.java
+++ src/test/java/uk/co/probablyfine/exercises/stopwatch/StopwatchTest.java
@@ -7,16 +7,53 @@ import static org.hamcrest.core.Is.is;
 
 public class StopwatchTest {
 
+    public static class TimeProvider {
+        private long duration;
+
+        public void advance(long seconds) {
+            this.duration += seconds;
+        }
+
+        public long time() {
+            return this.duration;
+        }
+    }
+
     public static class Stopwatch {
+        private final TimeProvider clock;
+
+        private long startTime;
+
+        public Stopwatch(TimeProvider clock) {
+            this.clock = clock;
+        }
+
         public String display() {
-            return "Current Time: 00:00";
+            return "Current Time: 00:0" + (clock.time() - startTime);
+        }
+
+        public void start() {
+            this.startTime = clock.time();
         }
     }
 
     @Test
     public void displayWithoutRunning() {
-        var stopwatch = new Stopwatch();
+        var stopwatch = new Stopwatch(new TimeProvider());
 
         assertThat(stopwatch.display(), is("Current Time: 00:00"));
+    }
+
+    @Test
+    public void displayAfterRunningFor1Second() {
+        var clock = new TimeProvider();
+
+        var stopwatch = new Stopwatch(clock);
+
+        stopwatch.start();
+
+        clock.advance(1L);
+
+        assertThat(stopwatch.display(), is("Current Time: 00:01"));
     }
 }
