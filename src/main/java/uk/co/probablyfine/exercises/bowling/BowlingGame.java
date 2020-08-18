b26dd81cc1d7c82e358279828fe0c9b43c126d7b
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2020-08-18 17:13:58 +0100

    refactor(bowling): Remove boolean state flags

--- src/main/java/uk/co/probablyfine/exercises/bowling/BowlingGame.java
+++ src/main/java/uk/co/probablyfine/exercises/bowling/BowlingGame.java
@@ -1,24 +1,24 @@
 package uk.co.probablyfine.exercises.bowling;
 
-public record BowlingGame(int score, int lookBackOne, int numberOfRolls, boolean lastFrameWasSpare, boolean lastFrameWasStrike, int lookBackTwo) {
+public record BowlingGame(int score, int numberOfRolls, int lookBackOne, int lookBackTwo) {
 
     public BowlingGame() {
-        this(0, 0, 1, false, false, 0);
+        this(0, 1, 0, 0);
     }
 
     public BowlingGame roll(int roll) {
         if (lookBackOne < 10 && (lookBackTwo + lookBackOne) == 10 && numberOfRolls % 2 == 1) {
-            return new BowlingGame(score + roll + roll, roll, numberOfRolls + 1, false, false, lookBackOne);
+            return new BowlingGame(score + roll + roll, numberOfRolls + 1, roll, lookBackOne);
         }
 
         if (lookBackTwo == 10 && numberOfRolls % 2 == 0) {
-            return new BowlingGame(score + lookBackOne + 2*roll, roll, numberOfRolls + 1, false, true, lookBackOne);
+            return new BowlingGame(score + lookBackOne + 2*roll, numberOfRolls + 1, roll, lookBackOne);
         }
 
         if (roll == 10 && numberOfRolls % 2 == 1) {
-            return new BowlingGame(score + roll, roll, numberOfRolls + 2, false, true, lookBackOne);
+            return new BowlingGame(score + roll, numberOfRolls + 2, roll, lookBackOne);
         }
 
-        return new BowlingGame(score + roll, roll, numberOfRolls + 1, lastFrameWasSpare, lastFrameWasStrike, lookBackOne);
+        return new BowlingGame(score + roll, numberOfRolls + 1, roll, lookBackOne);
     }
 }
