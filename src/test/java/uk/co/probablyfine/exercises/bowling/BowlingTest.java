5b736dcaf917464c4ecdf9a4515344b51a926f3b
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2020-08-18 17:44:02 +0100

    test(bowling): Add two example specs

--- src/test/java/uk/co/probablyfine/exercises/bowling/BowlingTest.java
+++ src/test/java/uk/co/probablyfine/exercises/bowling/BowlingTest.java
@@ -23,6 +23,16 @@ public class BowlingTest {
         assertThat(roll(10, 5, 4), is(10 + 9 + 9));
     }
 
+    @Test
+    public void exampleWithNoStrikesOrSpares() {
+        assertThat(roll(6, 3, 7, 1, 9, 0, 4, 4, 3, 2, 8, 1, 7, 2, 1, 6, 3, 5, 0, 8), is(80));
+    }
+
+    @Test
+    public void exampleWithStrikesAndSpares() {
+        assertThat(roll(7, 2, 7, 2, 3, 1, 6, 4, 7, 1, 4, 2, 10, 5, 3, 2, 7, 8, 1), is(97));
+    }
+
     public int roll(int... rolls) {
         return Arrays.stream(rolls)
                 .boxed()
