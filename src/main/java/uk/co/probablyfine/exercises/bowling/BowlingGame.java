e70ea8c04013748b7d8858b0079563b5679e8c2a
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2020-08-18 17:12:38 +0100

    refactor(bowling): Keep a look-back of two rather than flags about state

--- src/main/java/uk/co/probablyfine/exercises/bowling/BowlingGame.java
+++ src/main/java/uk/co/probablyfine/exercises/bowling/BowlingGame.java
@@ -1,28 +1,24 @@
 package uk.co.probablyfine.exercises.bowling;
 
-public record BowlingGame(int score, int frameScore, int numberOfRolls, boolean lastFrameWasSpare, boolean lastFrameWasStrike) {
+public record BowlingGame(int score, int lookBackOne, int numberOfRolls, boolean lastFrameWasSpare, boolean lastFrameWasStrike, int lookBackTwo) {
 
     public BowlingGame() {
-        this(0, 0, 1, false, false);
+        this(0, 0, 1, false, false, 0);
     }
 
     public BowlingGame roll(int roll) {
-        if (lastFrameWasSpare && numberOfRolls % 2 == 1) {
-            return new BowlingGame(score + roll + roll, 0, numberOfRolls + 1, false, false);
+        if (lookBackOne < 10 && (lookBackTwo + lookBackOne) == 10 && numberOfRolls % 2 == 1) {
+            return new BowlingGame(score + roll + roll, roll, numberOfRolls + 1, false, false, lookBackOne);
         }
 
-        if ((frameScore + roll) == 10 && numberOfRolls % 2 == 0) {
-            return new BowlingGame(score + roll, 0, numberOfRolls + 1, true, false);
+        if (lookBackTwo == 10 && numberOfRolls % 2 == 0) {
+            return new BowlingGame(score + lookBackOne + 2*roll, roll, numberOfRolls + 1, false, true, lookBackOne);
         }
 
         if (roll == 10 && numberOfRolls % 2 == 1) {
-            return new BowlingGame(score + roll, 0, numberOfRolls + 2, false, true);
+            return new BowlingGame(score + roll, roll, numberOfRolls + 2, false, true, lookBackOne);
         }
 
-        if (lastFrameWasStrike && numberOfRolls % 2 == 0) {
-            return new BowlingGame(score + frameScore + 2*roll, 0, numberOfRolls + 1, false, false);
-        }
-
-        return new BowlingGame(score + roll, numberOfRolls % 2 == 0 ? 0 : frameScore + roll, numberOfRolls + 1, lastFrameWasSpare, lastFrameWasStrike);
+        return new BowlingGame(score + roll, roll, numberOfRolls + 1, lastFrameWasSpare, lastFrameWasStrike, lookBackOne);
     }
 }
