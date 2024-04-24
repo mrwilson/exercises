c23b2a0520071a8ab5455e342e1231b5e21562c6
Alex Wilson <gpg@probablyfine.co.uk>
2024-04-24 15:04:31 +0100

    feat(jigsaw): Print two horizontal pieces

--- kata/src/test/java/uk/co/probablyfine/exercises/jigsaw/JigsawTest.java
+++ kata/src/test/java/uk/co/probablyfine/exercises/jigsaw/JigsawTest.java
@@ -21,10 +21,23 @@ public class JigsawTest {
          |__( )_|"""));
     }
 
+    @Test
+    void shouldReturnTwoPieces() {
+        assertThat(jigsaw(2, 1), is("""
+           _( )__ _( )__ 
+         _|     _|     _|
+        (_   _ (_   _ (_
+         |__( )_|__( )_|"""));
+    }
+
 
     private static String jigsaw(int width, int height) {
-        if (width > 0 && height > 0) {
+        if (width == 1) {
             return "   _( )__\n _|     _|\n(_   _ (_\n |__( )_|";
+        }
+
+        if (width == 2) {
+            return "   _( )__ _( )__\n _|     _|     _|\n(_   _ (_   _ (_\n |__( )_|__( )_|";
         }
         return "";
     }
