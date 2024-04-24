75b169f895aed1c06086feec175d1ae3a410b3fa
Alex Wilson <gpg@probablyfine.co.uk>
2024-04-24 14:59:12 +0100

    feat(jigsaw): Print single piece

--- kata/src/test/java/uk/co/probablyfine/exercises/jigsaw/JigsawTest.java
+++ kata/src/test/java/uk/co/probablyfine/exercises/jigsaw/JigsawTest.java
@@ -12,7 +12,20 @@ public class JigsawTest {
         assertThat(jigsaw(0, 0), is(""));
     }
 
+    @Test
+    void shouldReturnSinglePiece() {
+        assertThat(jigsaw(1, 1), is("""
+           _( )__  
+         _|     _|
+        (_   _ (_ 
+         |__( )_|"""));
+    }
+
+
     private static String jigsaw(int width, int height) {
+        if (width > 0 && height > 0) {
+            return "   _( )__\n _|     _|\n(_   _ (_\n |__( )_|";
+        }
         return "";
     }
 }
