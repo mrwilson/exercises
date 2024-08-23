2cd4d4fdc6ff75decde23f081aac8082f25c07f1
Alex Wilson <gpg@probablyfine.co.uk>
2024-08-23 12:08:20 +0100

    feat(doublets): Return both items if there is only one differing character

--- kata/src/test/java/uk/co/probablyfine/exercises/doublets/DoubletsTest.java
+++ kata/src/test/java/uk/co/probablyfine/exercises/doublets/DoubletsTest.java
@@ -3,7 +3,9 @@ package uk.co.probablyfine.exercises.doublets;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
 public class DoubletsTest {
@@ -13,7 +15,26 @@ public class DoubletsTest {
         assertThat(doublet("test", "test"), is(List.of("test")));
     }
 
+    @Test
+    void wordsOneLetterApartReturnsAListOfBothEntries() {
+        assertThat(doublet("head", "heal"), is(List.of("head", "heal")));
+    }
+
     private static List<String> doublet(String head, String tail) {
-        return List.of(head);
+        if (head.equals(tail)) {
+            return Collections.singletonList(head);
+        }
+
+        if (compare(head, tail) == 1) {
+            return List.of(head, tail);
+        }
+
+        throw new UnsupportedOperationException();
+    }
+
+    private static long compare(String head, String tail) {
+        return IntStream.range(0, head.length())
+                .filter(i -> head.charAt(i) != tail.charAt(i))
+                .count();
     }
 }
