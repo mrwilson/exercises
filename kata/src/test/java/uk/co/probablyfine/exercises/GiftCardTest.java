e5d8694e7c010aec1832085134f4c5081726e34c
Alex Wilson <gpg@probablyfine.co.uk>
2024-12-16 14:26:16 +0000

    feat(gift-cards): Employer gift cards

--- kata/src/test/java/uk/co/probablyfine/exercises/GiftCardTest.java
+++ kata/src/test/java/uk/co/probablyfine/exercises/GiftCardTest.java
@@ -3,6 +3,7 @@ package uk.co.probablyfine.exercises;
 import static java.util.Collections.emptyList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static uk.co.probablyfine.exercises.GiftCardTest.GiftCard.employer;
 import static uk.co.probablyfine.exercises.GiftCardTest.GiftCard.union;
 
 import java.util.Collections;
@@ -12,7 +13,8 @@ import org.junit.jupiter.api.Test;
 public class GiftCardTest {
 
     enum GiftCardType {
-        UNION
+        UNION,
+        EMPLOYER
     }
 
     record GiftCard(int amount, GiftCardType type) {
@@ -20,8 +22,16 @@ public class GiftCardTest {
             return new GiftCard(amount, GiftCardType.UNION);
         }
 
+        public static GiftCard employer(int amount) {
+            return new GiftCard(amount, GiftCardType.EMPLOYER);
+        }
+
         public double cost() {
-            return 0.94 * amount;
+            if (type == GiftCardType.UNION) {
+                return 0.94 * amount;
+            } else {
+                return 0.945 * amount;
+            }
         }
     }
 
@@ -37,9 +47,20 @@ public class GiftCardTest {
         assertThat(cards.get(0).cost(), is(250 * 0.94));
     }
 
+    @Test
+    void fallBackToNormalVouchers() {
+        var cards = giftCards(9);
+        assertThat(cards, is(List.of(employer(9))));
+        assertThat(cards.get(0).cost(), is(9 * 0.945));
+    }
+
     private List<GiftCard> giftCards(int value) {
         if (value == 250) {
-            return List.of(union(250));
+            return List.of(union(value));
+        }
+
+        if (value > 0) {
+            return List.of(employer(value));
         }
 
         return Collections.emptyList();
