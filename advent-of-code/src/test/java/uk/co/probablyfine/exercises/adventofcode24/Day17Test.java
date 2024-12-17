ca7bd7be184bbd7c9e07eff4d36727c7e66a9082
Alex Wilson <gpg@probablyfine.co.uk>
2024-12-17 11:14:27 +0000

    feat(aoc-2024-17): Halt, plus clean up some misimplementation

--- advent-of-code/src/test/java/uk/co/probablyfine/exercises/adventofcode24/Day17Test.java
+++ advent-of-code/src/test/java/uk/co/probablyfine/exercises/adventofcode24/Day17Test.java
@@ -1,5 +1,6 @@
 package uk.co.probablyfine.exercises.adventofcode24;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.is;
@@ -30,6 +31,10 @@ public class Day17Test {
             return new Computer(0, new ArrayList<>(), program, A, B, C);
         }
 
+        public boolean halted() {
+            return pointer >= program.size();
+        }
+
         public Computer tick() {
             var insn = program.get(pointer);
 
@@ -42,19 +47,20 @@ public class Day17Test {
             } else if (insn == 2) {
                 return new Computer(pointer + 2, output, program, A, combo() % 8, C);
             } else if (insn == 3) {
-                return new Computer(program.get(pointer + 1), output, program, A, B, C);
+                var next = A == 0 ? pointer + 2 : program.get(pointer + 1);
+                return new Computer(next, output, program, A, B, C);
             } else if (insn == 4) {
                 return new Computer(pointer + 2, output, program, A, B ^ C, C);
             } else if (insn == 5) {
-                output.add(combo());
-                return this;
+                output.add(combo() % 8);
+                return new Computer(pointer + 2, output, program, A, B, C);
             } else if (insn == 6) {
                 int newB = (int) (A / Math.pow(2, combo()));
                 return new Computer(pointer + 2, output, program, A, newB, C);
             } else if (insn == 7) {
-            int newC = (int) (A / Math.pow(2, combo()));
-            return new Computer(pointer + 2, output, program, A, B, newC);
-        }
+                int newC = (int) (A / Math.pow(2, combo()));
+                return new Computer(pointer + 2, output, program, A, B, newC);
+            }
 
             return this;
         }
@@ -82,7 +88,7 @@ public class Day17Test {
         var computer = parse(sampleInput());
 
         assertThat(computer.pointer(), is(0));
-        assertThat(computer.program(), is(Arrays.asList(0, 1, 5, 4, 3)));
+        assertThat(computer.program(), is(asList(0, 1, 5, 4, 3)));
         assertThat(computer.A(), is(729));
         assertThat(computer.B(), is(0));
         assertThat(computer.C(), is(0));
@@ -91,48 +97,66 @@ public class Day17Test {
 
     @Test
     void supportDivision() {
-        assertThat(setup(Arrays.asList(0, 1), 16, 0, 0).tick().A(), is(16 / 2));
-        assertThat(setup(Arrays.asList(0, 5), 16, 3, 0).tick().A(), is(16 / 8));
+        assertThat(setup(asList(0, 1), 16, 0, 0).tick().A(), is(16 / 2));
+        assertThat(setup(asList(0, 5), 16, 3, 0).tick().A(), is(16 / 8));
     }
 
     @Test
     void supportBxl() {
-        assertThat(setup(Arrays.asList(1, 8), 0, 3, 0).tick().B(), is(11));
+        assertThat(setup(asList(1, 8), 0, 3, 0).tick().B(), is(11));
     }
 
     @Test
     void supportBst() {
-        assertThat(setup(Arrays.asList(2, 2), 0, 2, 0).tick().B(), is(2));
-        assertThat(setup(Arrays.asList(2, 4), 197, 0, 0).tick().B(), is(5));
+        assertThat(setup(asList(2, 2), 0, 2, 0).tick().B(), is(2));
+        assertThat(setup(asList(2, 4), 197, 0, 0).tick().B(), is(5));
     }
 
     @Test
     void supportJnz() {
-        var computer = setup(Arrays.asList(3, 6, 0, 0, 0, 0, 2, 4), 197, 0, 0).tick();
+        var computer = setup(asList(3, 6, 0, 0, 0, 0, 2, 4), 197, 0, 0).tick();
         assertThat(computer.pointer(), is(6));
         assertThat(computer.tick().B(), is(5));
     }
 
     @Test
     void supportBxc() {
-        assertThat(setup(Arrays.asList(4, -1), 0, 3, 8).tick().B(), is(11));
+        assertThat(setup(asList(4, -1), 0, 3, 8).tick().B(), is(11));
     }
 
     @Test
     void supportOut() {
-        assertThat(setup(Arrays.asList(5, 3), 0, 0, 0).tick().output(), is(singletonList(3)));
-        assertThat(setup(Arrays.asList(5, 6), 0, 0, 10).tick().output(), is(singletonList(10)));
+        assertThat(setup(asList(5, 3), 0, 0, 0).tick().output(), is(singletonList(3)));
+        assertThat(setup(asList(5, 6), 0, 0, 10).tick().output(), is(singletonList(2)));
     }
 
     @Test
     void supportBdv() {
-        assertThat(setup(Arrays.asList(6, 1), 16, 0, 0).tick().B(), is(16 / 2));
-        assertThat(setup(Arrays.asList(6, 5), 16, 3, 0).tick().B(), is(16 / 8));
+        assertThat(setup(asList(6, 1), 16, 0, 0).tick().B(), is(16 / 2));
+        assertThat(setup(asList(6, 5), 16, 3, 0).tick().B(), is(16 / 8));
     }
 
     @Test
     void supportCdv() {
-        assertThat(setup(Arrays.asList(7, 1), 16, 0, 0).tick().C(), is(16 / 2));
-        assertThat(setup(Arrays.asList(7, 5), 16, 3, 0).tick().C(), is(16 / 8));
+        assertThat(setup(asList(7, 1), 16, 0, 0).tick().C(), is(16 / 2));
+        assertThat(setup(asList(7, 5), 16, 3, 0).tick().C(), is(16 / 8));
+    }
+
+    @Test
+    void examples() {
+        assertThat(run(setup(asList(2, 6), 0, 0, 9)).B(), is(1));
+        assertThat(run(setup(asList(5, 0, 5, 1, 5, 4), 10, 0, 0)).output(), is(asList(0, 1, 2)));
+        assertThat(
+                run(setup(asList(0, 1, 5, 4, 3, 0), 2024, 0, 0)).output(),
+                is(asList(4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0)));
+        assertThat(run(setup(asList(1, 7), 0, 29, 0)).B(), is(26));
+        assertThat(run(setup(asList(4, 0), 0, 2024, 43690)).B(), is(44354));
+    }
+
+    public static Computer run(Computer computer) {
+        while (!computer.halted()) {
+            computer = computer.tick();
+        }
+        return computer;
     }
 }
